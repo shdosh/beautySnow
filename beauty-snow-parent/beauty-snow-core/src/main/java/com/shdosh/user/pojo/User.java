a66845fdd80e7464644bbbb374f3a223c0e81330
shidongsheng <shidongsheng@supcon.com>
2018-06-06 18:41:27 +0800

    User实现Serializable接口，网络传递需要序列化

--- beauty-snow-parent/beauty-snow-core/src/main/java/com/shdosh/user/pojo/User.java
+++ beauty-snow-parent/beauty-snow-core/src/main/java/com/shdosh/user/pojo/User.java
@@ -1,8 +1,9 @@
 package com.shdosh.user.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class User {
+public class User implements Serializable {
     private Integer id;
     
     private String userName;
